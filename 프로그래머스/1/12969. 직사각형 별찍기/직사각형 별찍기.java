724f059c4520fe4abbb78ec4fbf5a1e01ccd3aa2
MinYeong-mkk <64885719+MinYeong-mkk@users.noreply.github.com>
2024-07-18 19:50:04 +0900

    [level 1] Title: 직사각형 별찍기, Time: 322.36 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -5,14 +5,16 @@ class Solution {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        
-        for(int i=0;i<n;i++) sb1.append("*");
-        for(int i=0;i<m;i++) sb2.append(sb1+"\n");
-        System.out.println(sb2.toString());
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
     }
 }
