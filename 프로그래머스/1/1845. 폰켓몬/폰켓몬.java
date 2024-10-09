9f2631f97e06a76d126dc9196f6fa4321621c64f
MinYeong-mkk <64885719+MinYeong-mkk@users.noreply.github.com>
2024-10-10 00:07:57 +0900

    [level 1] Title: 폰켓몬, Time: 6.22 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,9 +1,9 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for(int n : nums) set.add(n);
-        return (int)Math.min(set.size(), nums.length/2);
+        for(int i=0;i<nums.length;i++) set.add(nums[i]);
+        
+        return set.size()>nums.length/2 ? nums.length/2:set.size(); 
     }
 }
