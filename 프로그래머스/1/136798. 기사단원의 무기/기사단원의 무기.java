7851860c8810becb4a50b53079aa367d96c75448
MinYeong-mkk <64885719+MinYeong-mkk@users.noreply.github.com>
2024-10-10 00:28:35 +0900

    [level 1] Title: 기사단원의 무기, Time: 9497.43 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,22 +1,20 @@
-import java.util.*;
-
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        for(int i=1; i<=number; i++){
-            int x = 약수(i);
-            if(x > limit) x = power;
-            System.out.print(x + " ");
-            answer+=x;
+        int nums[] = new int[number+1];
+        int cnt=2;
+        int sum=0;
+        nums[1]=1;
+        for(int i=2;i<=number;i++){
+            for(int j=2;j<=i/2;j++){
+                if(i%j==0) cnt++;
+            }
+            if(cnt>limit) nums[i]=power;
+            else nums[i]=cnt;
+            cnt=2;
         }
-        return answer;
-    }
-    public static int 약수(int n){
-        if(n == 1) return 1;
-        int cnt = 0;
-        int rootN = (int)Math.ceil(Math.sqrt(n));
-        for(int i=2; i<rootN; i++) if(n%i==0) cnt+=2;
-        if(rootN*rootN == n) cnt++;
-        return cnt+2;
+        for(int i=0;i<=number;i++){
+            sum+=nums[i];
+        }
+        return sum;
     }
 }
