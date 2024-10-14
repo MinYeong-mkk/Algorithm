2eda2901f23e26988ffd9833e1cd99f42dea0593
MinYeong-mkk <64885719+MinYeong-mkk@users.noreply.github.com>
2024-10-14 22:07:16 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -3,19 +3,17 @@ class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
         int arr[] = {6,6,5,4,3,2,1};
+        HashMap<Integer, Boolean> map = new HashMap<>();
         
-        ArrayList<Integer> al = new ArrayList<>();
         int win = 0;
         int num0 = 0;
-        for(int num:lottos){
-            if(num!=0){
-                al.add(num);
-            }else num0++;
-        }
+        
         for(int num:win_nums){
-            if(al.contains(num)) {
-                win++;
-            }
+            map.put(num, true);
+        }
+        for(int num:lottos){
+            if(map.containsKey(num)) win++;
+            if(num == 0) num0++;
         }
         
         answer[0] = arr[win+num0];
