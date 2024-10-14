4513461bc6fe2d89a8217fd0a2f5dd808e9c2243
MinYeong-mkk <64885719+MinYeong-mkk@users.noreply.github.com>
2024-10-14 22:43:26 +0900

    [level 2] Title: 귤 고르기, Time: 96.17 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,14 +1,23 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
-        Integer arr[] = new Integer[10000001];
-        Arrays.fill(arr, 0);
-        for(int v : tangerine) arr[v]++;
-        Arrays.sort(arr, (o1,o2) -> o2-o1);
+        ArrayList<Integer> list = new ArrayList<>();
+        Arrays.sort(tangerine);
         
+        int temp = 0;
+        int 압좌 = -1; // 압축될 좌표
+        for(int i=0; i<tangerine.length; i++){
+            if(temp == tangerine[i]) list.set(압좌, list.get(압좌)+1);
+            else{
+                list.add(1);
+                압좌++;
+                temp = tangerine[i];
+            }
+        }
+        Collections.sort(list, (o1,o2) -> o2-o1);
         int index = 0;
         while(k>0){
-            k-=arr[index];
+            k-=list.get(index);
             index++;
         }
         return index;
