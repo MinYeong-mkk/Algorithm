2d527d8a20a392c15152e4f7aaef6d7fd8b783de
MinYeong-mkk <64885719+MinYeong-mkk@users.noreply.github.com>
2024-10-16 21:14:07 +0900

    [Bronze II] Title: 영식이와 친구들, Time: 64 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws Exception{
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());//사람 수
+		int M = Integer.parseInt(st.nextToken());//공을 받는 횟수
+		int L = Integer.parseInt(st.nextToken());//공을 받는 사람의 거리
+		int cnt = 0;
+		int[] round = new int[N];
+		int i = 0;
+		while(M!=Max(round)) {
+			round[i]++;
+			if(round[i]%2==0) i-=L;
+			else i += L;
+			
+			if(i>=N) i-=N;
+			else if(i<0) i+=N;
+			cnt++;
+		}
+		System.out.println(cnt-1);
+	}
+	
+	public static int Max(int[] arr) {
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i]>max) max = arr[i];
+		}
+		return max;
+	}
+
+}
