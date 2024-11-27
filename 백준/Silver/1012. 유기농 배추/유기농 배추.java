3e03f2c70f718596747f09b8f65320ed2954b683
MinYeong-mkk <64885719+MinYeong-mkk@users.noreply.github.com>
2024-11-27 19:48:50 +0900

    [Silver II] Title: 유기농 배추, Time: 84 ms, Memory: 13268 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int M, N, K, count;
+    static boolean[][] visited;
+    static int[][] field;
+    static int[] dx = {-1, 0, 0, 1};
+    static int[] dy = {0, -1, 1, 0};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            field = new int[M][N];
+            visited = new boolean[M][N];
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                field[x][y] = 1;
+            }
+
+            count = 0;
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (field[i][j] == 1 && !visited[i][j]) {
+                        dfs(i, j);
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < M && ny < N) {
+                if (field[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+}
